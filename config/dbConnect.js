1af1ce837d15a25f6234c17cd4aa947e9b50d4f2
louisa <mensahlouuisa39@gmail.com>
2022-04-20 14:36:54 +0000

    Shop-API

---
+++ config/dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// create a function to connect to the database
+const dbConnect = async () => {
+  try {
+    // connecting to the database
+    await mongoose.connect("mongodb://127.0.0.1:27017/shop");
+    console.log("Database connected...");
+  } catch (error) {
+    // throw error for debuging
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = dbConnect;
---
+++ routes/products.router.js
@@ -0,0 +1,19 @@
+const {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+} = require("../controllers/products.controller");
+
+const { Router } = require("express");
+const productRouter = Router();
+
+productRouter.route("/").get(getAllProducts).post(createProduct);
+productRouter
+  .route("/:productId")
+  .get(getSingleProduct)
+  .patch(updateProduct)
+  .delete(deleteProduct);
+
+module.exports = productRouter;
