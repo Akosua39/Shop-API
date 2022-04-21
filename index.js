7798a68543eb33e0e6a16d2c993193b1b3b7a027
louisa <mensahlouuisa39@gmail.com>
2022-04-21 13:53:59 +0000

    auth

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const productRouter = require("./routes/products.router");
+const authRouter = require("./routes/auth.router");
 
 const app = express();
 app.use(express.json());
 
 app.use("/products", productRouter);
+app.use("/auth", authRouter);
 
 const start = async () => {
   await dbConnect();
---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+      length: 6,
+    },
+  },
+  {
+    timestsamps: true,
+  }
+);
+module.exports = model("User", userSchema);
